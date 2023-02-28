61dedf2551b64cb2ed97c9feba1ea586b20a8bb3
sofiaseq15 <126524066+sofiaseq15@users.noreply.github.com>
2023-02-27 21:14:23 -0300

    initial commit

---
+++ java.js
@@ -0,0 +1,7 @@
+document.getElementById('Mostrar').addEventListener ('click', function(){
+document.getElementById('Video').style.display = 'block'
+})
+
+document.getElementById('Ocultar').addEventListener ('click', function(){
+document.getElementById('Video').style.display = 'none'
+})
